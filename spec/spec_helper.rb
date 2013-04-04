117b2a629d67b33c7d7af5864453c5dbc38f8619
Martin Jackson <martin@uncommonsense-uk.com>
2013-04-04 12:35:58 +0100

    Added additional help assets

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,7 @@
+require 'puppet'
+require 'rspec'
 require 'rspec-puppet'
+require 'puppetlabs_spec_helper/module_spec_helper'
 
 fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
 
